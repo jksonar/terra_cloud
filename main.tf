1eba46f1ef60aa9d62f2a44294d78f40dfd062e8
jksonar <jksonar1992@gmail.com>
2024-03-06 15:57:57 +0530

    creating instance

--- main.tf
+++ main.tf
@@ -1,2 +1,23 @@
 # login terraform cloud using command 
 # terraform login
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["RHEL-9.3.0_HVM-20240117-arm64-49-Hourly2-GP3"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["309956199498"] 
+}
+
+resource "aws_instance" "example_server" {
+  ami           = "ami-04e914639d0cca79a"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "terraform-ec2"
+  }
+}
