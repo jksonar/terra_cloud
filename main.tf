f5f55d0620e908ccd549a887bbb81cb5e26fb0bd
jksonar <jksonar1992@gmail.com>
2024-03-06 16:14:40 +0530

    changes done to use redhat ami

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 # login terraform cloud using command 
 # terraform login
 
-data "aws_ami" "ubuntu" {
+data "aws_ami" "redhat_ami" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["RHEL-9.3.0_HVM-20240117-arm64-49-Hourly2-GP3"]
+    values = ["RHEL-9.3.0_HVM-20240117-x86_64-49-Hourly2-GP3"]
   }
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-  owners = ["309956199498"] 
+  owners = ["309956199498"]
 }
 
 resource "aws_instance" "example_server" {
-  ami           = "ami-04e914639d0cca79a"
+  ami           = data.aws_ami.redhat_ami.id
   instance_type = "t2.micro"
   tags = {
     Name = "terraform-ec2"
